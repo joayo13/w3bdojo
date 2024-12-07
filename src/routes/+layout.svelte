884a113e1a71274ffc1fec794f535fa083a755bf
Jordan Ayotte <jordanayotte13@outlook.com>
2024-12-07 00:56:06 -0500

    built out two nav components for layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,18 @@
 	let { children } = $props();
 </script>
 
+<nav class="fixed bg-neutral-900 h-16 w-full z-10">
+	
+</nav>
+<nav aria-labelledby="lessons-header" class="hidden md:flex flex-col fixed w-80 pt-16 px-2 bg-neutral-200 h-screen">
+	<h2 class="text-xl" id="lessons-header">Lessons</h2>
+	<ul class="flex flex-col gap-2">
+		<li>
+			<a href="/">Home</a>
+		</li>
+	</ul>
+</nav>
+<div class="md:pl-80 pt-16 bg-neutral-50 text-neutral-900 flex justify-center">
 {@render children()}
+</div>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<section class="px-2 py-8 max-w-3xl">
+    <h1 class="text-4xl">Web Accessibility Interactive Tutorial</h1>
+    <h2 class="text-2xl mt-8">What is it?</h2>
+    <p class="mt-2 text-xl">This tutorial blah blah blah Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores culpa quis expedita, suscipit distinctio repellendus repudiandae ducimus quisquam aperiam nemo odit voluptatem sequi reiciendis deleniti unde corporis. Commodi, quis ut!</p>
+</section>
