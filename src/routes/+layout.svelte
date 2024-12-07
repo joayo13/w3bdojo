2e7b4cc6036ed571941cc7b5fc5d4ef1dfacc95a
Jordan Ayotte <jordanayotte13@outlook.com>
2024-12-07 01:07:53 -0500

    updated some spacing values again

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,15 +6,18 @@
 <nav class="fixed bg-neutral-900 h-16 w-full z-10">
 	
 </nav>
-<nav aria-labelledby="lessons-header" class="hidden md:flex flex-col fixed w-80 pt-16 px-2 bg-neutral-200 h-screen">
-	<h2 class="text-xl" id="lessons-header">Lessons</h2>
-	<ul class="flex flex-col gap-2">
+<nav aria-labelledby="lessons-header" class="hidden md:flex flex-col fixed w-80 h-screen pt-16 px-2 bg-neutral-200 text-right">
+	<h2 class="text-2xl mt-16" id="lessons-header">Lessons</h2>
+	<ul class="flex flex-col gap-8 text-xl mt-8">
 		<li>
-			<a href="/">Home</a>
+			<a href="/lesson-1">Lesson 1: Keyboard Accessibility</a>
+		</li>
+		<li>
+			<a href="/lesson-1">Lesson 2: ARIA 101</a>
 		</li>
 	</ul>
 </nav>
-<div class="md:pl-80 pt-16 bg-neutral-50 text-neutral-900 flex justify-center">
+<div class="md:pl-80 pt-32 bg-neutral-50 text-neutral-900 flex justify-center">
 {@render children()}
 </div>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<section class="px-2 py-8 max-w-3xl">
+<section class="px-2 max-w-3xl">
     <h1 class="text-4xl">Web Accessibility Interactive Tutorial</h1>
     <h2 class="text-2xl mt-8">What is it?</h2>
     <p class="mt-2 text-xl">This tutorial blah blah blah Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores culpa quis expedita, suscipit distinctio repellendus repudiandae ducimus quisquam aperiam nemo odit voluptatem sequi reiciendis deleniti unde corporis. Commodi, quis ut!</p>
