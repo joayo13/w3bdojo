4bceb71c345f85caf4cce9c202fe57e99e73ece7
Jordan Ayotte <jordanayotte13@outlook.com>
2024-12-08 12:34:06 -0500

    added actual colors for dark and light

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,12 +18,13 @@
 	}
 </script>
 
-<nav class="fixed flex items-center shadow-sm z-10 h-16 w-full bg-neutral-50 dark:bg-neutral-900">
-	<button onclick={toggleDarkMode} aria-label="Toggle dark mode" aria-pressed={darkMode} class="border rounded-lg border-neutral-500 hover:border-neutral-400 bg-moon-svg dark:bg-sun-svg size-12"></button>
+<nav class="fixed flex items-center justify-between shadow-sm z-10 h-16 w-full px-2 bg-neutral-100 dark:bg-neutral-900 text-neutral-900 dark:text-neutral-200">
+	<p class="text-xl">W.A.I.T</p>
+	<button onclick={toggleDarkMode} aria-label="Toggle dark mode" aria-pressed={darkMode} class=" border rounded-lg border-neutral-500 hover:border-neutral-400 bg-moon-svg dark:bg-sun-svg size-12"></button>
 </nav>
 <nav
 	aria-labelledby="lessons-header"
-	class="fixed hidden h-screen w-80 flex-col bg-neutral-200 px-2 pt-16 text-right md:flex"
+	class="fixed hidden h-screen w-80 flex-col bg-neutral-200 dark:bg-neutral-800 text-neutral-900 dark:text-neutral-200 px-2 pt-16 text-right md:flex"
 >
 	<h2 class="mt-14 text-2xl" id="lessons-header">Lessons</h2>
 	<ul class="mt-8 flex flex-col gap-8 text-xl">
@@ -35,6 +36,6 @@
 		</li>
 	</ul>
 </nav>
-<div class="flex justify-center bg-neutral-50 pt-32 text-neutral-900 md:pl-80">
+<div class="flex justify-center bg-neutral-50 dark:bg-neutral-950 pt-32 text-neutral-900 dark:text-neutral-200 min-h-screen md:pl-80">
 	{@render children()}
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,4 +6,10 @@
 		culpa quis expedita, suscipit distinctio repellendus repudiandae ducimus quisquam aperiam nemo
 		odit voluptatem sequi reiciendis deleniti unde corporis. Commodi, quis ut!
 	</p>
+	<h2 class="mt-8 text-2xl">Why should I care?</h2>
+	<p class="mt-2 text-xl">
+		This tutorial blah blah blah Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores
+		culpa quis expedita, suscipit distinctio repellendus repudiandae ducimus quisquam aperiam nemo
+		odit voluptatem sequi reiciendis deleniti unde corporis. Commodi, quis ut!
+	</p>
 </section>
