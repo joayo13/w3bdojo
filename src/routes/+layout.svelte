12fa3e28cc3a76f226f1ee4a33d0bbc2ce7a4a75
Jordan Ayotte <jordanayotte13@outlook.com>
2024-12-07 23:36:55 -0500

    added dark mode toggle

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,27 +1,31 @@
 <script lang="ts">
 	import '../app.css';
-	import DarkModeButton from '../components/DarkModeButton.svelte';
-	import LightModeButton from '../components/LightModeButton.svelte';
 	let darkMode = $state(false);
 	let { children } = $props();
 
-	function toggleMode() {
+
+
+	function toggleDarkMode() {
 		darkMode = !darkMode;
+		if (darkMode === true) {
+			localStorage.theme = "dark";
+		}
+		else {
+			localStorage.theme = "light";
+		}
+		document.documentElement.classList.toggle('dark', localStorage.theme === 'dark')
+
 	}
 </script>
 
-<nav class="fixed z-10 h-16 w-full bg-neutral-900">
-	{#if darkMode === true}
-		<DarkModeButton {toggleMode} />
-	{:else}
-		<LightModeButton {toggleMode} />
-	{/if}
+<nav class="fixed flex items-center shadow-sm z-10 h-16 w-full bg-neutral-50 dark:bg-neutral-900">
+	<button onclick={toggleDarkMode} aria-label="Toggle dark mode" aria-pressed={darkMode} class="border rounded-sm border-neutral-500 hover:border-neutral-400 bg-moon-svg dark:bg-sun-svg size-12"></button>
 </nav>
 <nav
 	aria-labelledby="lessons-header"
 	class="fixed hidden h-screen w-80 flex-col bg-neutral-200 px-2 pt-16 text-right md:flex"
 >
-	<h2 class="mt-16 text-2xl" id="lessons-header">Lessons</h2>
+	<h2 class="mt-14 text-2xl" id="lessons-header">Lessons</h2>
 	<ul class="mt-8 flex flex-col gap-8 text-xl">
 		<li>
 			<a href="/lesson-1">Lesson 1: Keyboard Accessibility</a>
