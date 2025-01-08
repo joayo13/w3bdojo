5ae24dc5dc3793b251dfa4be48a99e3202705383
Jordan Ayotte <jordanayotte13@outlook.com>
2025-01-08 14:05:25 -0500

    improvement to darkmode/lightmode. now its a slider

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -50,10 +50,7 @@
 	class="fixed w-80 hidden h-screen flex-col bg-neutral-200 dark:bg-neutral-800 text-neutral-900 dark:text-neutral-200 px-4 pt-16 text-right md:flex"
 >
 	<h2 class="mt-14 text-2xl" id="lessons-header">Lessons</h2>
-	<ul class="mt-8 flex flex-col gap-8 text-xl">
-		<li>
-			<a href="/lesson-1">Lesson 0: Introduction</a>
-		</li>
+	<ul class="mt-8 flex flex-col gap-8 text-xl relative">
 		<li>
 			<a href="/lesson-1">Lesson 1: Keyboard Accessibility</a>
 		</li>
@@ -63,8 +60,12 @@
 		<li>
 			<a href="/lesson-1">Lesson 3: REMS</a>
 		</li>
+		<div class="h-px w-full bg-neutral-900 dark:bg-neutral-200"></div>
+	<button onclick={toggleDarkMode} aria-label="Toggle dark mode" aria-pressed={darkMode} class="w-20 ml-auto h-8 border border-neutral-900 dark:border-neutral-200 rounded-full relative">
+		<div class="absolute bg-moon-svg dark:bg-sun-svg size-8 right-0 top-1/2 -translate-y-1/2 dark:left-0"></div>
+	</button>
 	</ul>
-	<button onclick={toggleDarkMode} aria-label="Toggle dark mode" aria-pressed={darkMode} class="bg-moon-svg dark:bg-sun-svg size-10"></button>
+	
 </nav>
 <!-- /desktop nav links -->
  <!-- mobile nav links -->
@@ -72,13 +73,15 @@
  <nav
  aria-labelledby="lessons-header"
  transition:slide={{ delay: 100, duration: 300, easing: quintOut, axis: 'x' }}
- class="fixed right-0  whitespace-nowrap h-screen w-80 flex-col bg-neutral-200 dark:bg-neutral-800 text-neutral-900 dark:text-neutral-200 px-4 pt-16 text-left"
+ class="fixed right-0 z-10 whitespace-nowrap h-screen w-80 flex-col bg-neutral-200 dark:bg-neutral-800 text-neutral-900 dark:text-neutral-200 px-4 text-left"
 >
+<button class="absolute top-2 right-2" aria-label="exit-hamburger" onclick={toggleMobileNavLinks}>
+	<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-12">
+		<path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+	  </svg>	  
+</button>
  <h2 class="mt-14 text-2xl" id="lessons-header">Lessons</h2>
  <ul class="mt-8 flex flex-col gap-8 text-xl">
-	 <li>
-		 <a href="/lesson-1">Lesson 0: Introduction</a>
-	 </li>
 	 <li>
 		 <a href="/lesson-1">Lesson 1: Keyboard Accessibility</a>
 	 </li>
@@ -88,8 +91,11 @@
 	 <li>
 		 <a href="/lesson-1">Lesson 3: REMS</a>
 	 </li>
+	 <div class="h-px w-full bg-neutral-900 dark:bg-neutral-200"></div>
+	<button onclick={toggleDarkMode} aria-label="Toggle dark mode" aria-pressed={darkMode} class="w-20 h-8 border border-neutral-900 dark:border-neutral-200 rounded-full relative">
+		<div class="absolute bg-moon-svg dark:bg-sun-svg size-8 right-0 top-1/2 -translate-y-1/2 dark:left-0"></div>
+	</button>
  </ul>
- <button onclick={toggleDarkMode} aria-label="Toggle dark mode" aria-pressed={darkMode} class="bg-moon-svg dark:bg-sun-svg size-10"></button>
 </nav>
 {/if}
  <!-- /mobile nav links -->
